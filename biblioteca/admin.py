6eb0766365a1e3daa6cb326ccf3cee1eaf64f145
Salvador Gibert <salvigibert@gmail.com>
2020-08-20 12:41:26 -0300

    Toques finales

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -2,17 +2,54 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+class PrestamoInLine(admin.TabularInline):  # Para ver los prestamos de la persona
+   model = Prestamo
+
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+   inlines = [PrestamoInLine, ]
+   list_display = ('matricula', 'apellido',)
+   search_fields = ['nombre', 'apellido', 'correo', 'telefono', 'matricula']
+   fieldsets = (
+      ("Persona", {
+         'fields': ('nombre', 'apellido',)
+      }),
+      ('Contacto', {
+         'fields': ('correo', 'telefono',)
+      }),
+      ('Alumno', {
+         'fields': ('matricula',)
+      }),
+      ('Bibloteca', {
+         'fields': ('numLibros', 'adeudo',)
+      }),
+   )
+
+
 @admin.register(Materiale)
-class Material (admin.ModelAdmin):
+class MaterialAdm (admin.ModelAdmin):
    list_display = ["codigo" ,"titulo", "autor"]
 
 @admin.register(Prestamo)
-class Prestamo (admin.ModelAdmin):
+class PrestamoAdm (admin.ModelAdmin):
    list_display = ["fechaSalida", "fechaRegreso"]
 
 @admin.register(Personia)
-class Persona (admin.ModelAdmin):
-   list_display = ["nombre", "apellido", "telefono"]
+class PersonaAdm (admin.ModelAdmin):
+   list_display = ["nombres", "apellido", "telefono"]
+
+@admin.register(Libro)
+class LibroAdm(admin.ModelAdmin):
+   list_display = ('titulo', 'autor',)
+   list_filter = ('autor', 'anio',)
+   fieldsets = (
+      ("Material y Editorial", {
+         'fields': ('codigo', 'autor', 'titulo', 'anio', "editorial" )
+      }),
+      ('Estado', {
+         'fields': ('status',)
+      }),
+   )
 
 
 
--- biblioteca/models.py
+++ biblioteca/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+OPCIONES = [
+        ('LI', 'Librado'),
+        ('PR', 'Prestado'),
+        ('AR', 'Arreglandose'),
+    ]
 
 class Materiale(models.Model):
     tipoMaterial = models.CharField(max_length = 20)
@@ -9,32 +14,57 @@ class Materiale(models.Model):
     autor = models.CharField(max_length = 20)
     titulo = models.CharField(max_length = 100)
     anio = models.DateField()
+    status = models.CharField(
+        max_length=2,
+        choices=OPCIONES,
+        default='LI',
+    )
 
     def __str__(self):
         return str(self.titulo)
 
-class Prestamo(models.Model):
-    codigo = models.CharField(max_length = 20)
-    idd = models.CharField(max_length = 20)
-    fechaSalida= models.CharField(max_length = 20)
-    fechaRegreso = models.CharField(max_length = 20)
-
-
 class Personia(models.Model):
     tipoPersona = models.CharField(max_length = 20)
-    nombre = models.CharField(max_length = 20)
+    nombres = models.CharField(max_length = 20)
     apellido = models.CharField(max_length = 20)
     correo = models.CharField(max_length = 21)
     telefono = models.CharField(max_length = 20)
     numLibros = models.CharField(max_length = 20)
     aduedo = models.CharField(max_length = 20)
 
-class Libro(models.Model):
+    def __str__(self):
+        return "{} {}".format(self.nombres,self.apellido)
+
+class Prestamo(models.Model):
+    codigo = models.CharField(max_length = 20)
+    idd = models.CharField(max_length = 20)
+    fechaSalida= models.CharField(max_length = 20)
+    fechaRegreso = models.CharField(max_length = 20)
+    person = models.ForeignKey("Personia",on_delete=models.CASCADE,
+        null=False)
+
+    def __str__(self):
+        return str(self.codigo)
+
+
+
+class Libro(Materiale):
     editorial = models.CharField(max_length = 20)
 
+    def __str__(self):
+        return str(self.editorial)
 
-class Alumno(models.Model):
-    matricula = models.CharField(max_length = 20)
+class Revista(Materiale):
+    pass
+
+class Alumno(Personia):
+    matricula = models.IntegerField()
+
+    def __str__(self):
+        return str(self.matricula)
 
 class Profesore(models.Model):
-    numEmpleado = models.CharField(max_length = 20)
+    numEmpleado = models.IntegerField()
+
+    def __str__(self):
+        return str(self.numEmpleado)
